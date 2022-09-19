c7daa9af428dead061a45ce50d5c7cc3c16e6dd6
daichi1008 <forester.dn1008@gmail.com>
2022-09-19 11:37:56 +0900

    講座⑤

---
+++ JS/scrpt.js
@@ -0,0 +1,28 @@
+
+console.log(5+7)
+
+
+console.log(10-6);
+
+
+console.log(20*5);
+
+
+console.log(100/3);
+
+console.log(100%3)
+
+const x= 3;
+const y=4;
+
+console.log(x+y);
+
+console.log("背番号"+"7")
+console.log("背番号"+7)
+
+// コメントアウト
+/*コメントアウト
+コメントアウト
+コメントアウト
+コメントアウト
+コメントアウト*/
